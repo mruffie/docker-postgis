058404f9cb4cef492c7a1b8ce2e2e6a0fc9c3f4c
mruffie <ruffie.marc@gmail.com>
2023-08-03 09:36:31 +0200

    Update setup-vpn.sh

--- scripts/setup-vpn.sh
+++ scripts/setup-vpn.sh
@@ -1,4 +1,18 @@
 #!/usr/bin/env bash
 
+# Verifiez que le fichier de configuration OpenVPN existe
+if [ ! -f "/etc/openvpn/vpn.conf" ]; then
+    echo "Le fichier de configuration OpenVPN n'existe pas."
+    exit 1
+fi
+# Verifiez que le fichier de userpass existe
+if [ ! -f "/etc/openvpn/userpass.txt" ]; then
+    echo "Le fichier userpass OpenVPN n'existe pas."
+    exit 1
+fi
+
 # Start OpenVPN
 openvpn --config /etc/openvpn/vpn.conf --auth-user-pass /etc/openvpn/userpass.txt --daemon
+
+# Affichez un message de succès
+echo "OpenVPN lancement fait"
