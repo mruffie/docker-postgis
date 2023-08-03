50d501d5901a025321caf354c9f70c30fcd0c894
mruffie <ruffie.marc@gmail.com>
2023-08-03 11:07:49 +0200

    Update setup-vpn.sh

--- scripts/setup-vpn.sh
+++ scripts/setup-vpn.sh
@@ -12,7 +12,7 @@ if [ ! -f "/etc/openvpn/userpass.txt" ]; then
 fi
 echo "Les fichier de configuration OpenVPN existent, je lance."
 # Start OpenVPN
-openvpn --config /etc/openvpn/vpn.conf
+openvpn --config /etc/openvpn/vpn.conf --cipher AES-256-CBC
 
 # Affichez un message de succès
 echo "OpenVPN lancement fait"
